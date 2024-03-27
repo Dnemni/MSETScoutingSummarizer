37cc4df334354033a2727796658d55e452ffa504
Dhruv <dhruvnemani@gmail.com>
2024-03-27 11:47:51 -0700

    data check

--- summarizer_app.py
+++ summarizer_app.py
@@ -51,79 +51,8 @@ class SideBarSetup:
         return t
 
 
-def basicTeamBoxPlot(tmevscr):
-    #Charts
-    df = pd.DataFrame([(event, score) for event, scores in tmevscr.items() for score in scores], columns=['Event', 'Points Scored'])
+AppData = pd.read_csv("DATA.csv")
 
-    boxplot = alt.Chart(df).mark_boxplot(extent="min-max", size = 50).encode(
-        alt.X("Event:N", axis=alt.Axis(labels=True, ticks=True, domain=True, grid=True, domainColor="white", gridColor="white", labelColor="black", tickColor="white", titleColor="black")),
-        alt.Y("Points Scored:Q", axis=alt.Axis(labels=True, ticks=True, domain=True, grid=True, domainColor="white", gridColor="white", labelColor="black", tickColor="white", titleColor="black")).scale(zero=False),
-        alt.Color("Event:N").legend(None),
-        ).properties(
-            width=400,
-            height=300
-        ).configure_title(
-            fontSize=16,
-            anchor='start'
-        )
-    # Display the boxplot
-    st.altair_chart(boxplot, use_container_width=True)
-    
-    
-def individualTeamScatterPlot(scores_data):
-    
-    for event, scores in scores_data.items():
-        st.write("Event: ", event)
-        min_length = min(len(scores[0]), len(scores[1]))
-    
-        matchnames, alliances = getEventAlliances(tm, tmy, event)
-        # Prepare data for scatter plot
-        data = pd.DataFrame({
-          'Match': range(1, min_length + 1),
-          'Actual Score': scores[0][:min_length],
-          'Predicted Score': scores[1][:min_length],
-          'Match Name': matchnames,
-          'Alliance': alliances
-        })
-        
-        # Create scatter plot
-        #data = pd.DataFrame({'Match': range(1, len(scores) + 1), 'Points Scored': scores})
-
-        scatter_plot_1 = alt.Chart(data).mark_circle(size=60).encode(
-            alt.X("Match:N", axis=alt.Axis(labels=True, ticks=True, domain=True, grid=True, domainColor="white", gridColor="white", labelColor="black", tickColor="white", titleColor="black")),
-            alt.Y("Actual Score:Q", axis=alt.Axis(labels=True, ticks=True, domain=True, grid=True, domainColor="white", gridColor="white", labelColor="black", tickColor="white", titleColor="black")).scale(zero=False),
-            color = alt.value("blue"),
-            tooltip = ['Match', 'Match Name', 'Actual Score', 'Alliance'] 
-            #alt.Color("variable:N", legend=alt.Legend(title="Score Type")),
-            ).properties(
-                width=200,
-                height=300
-            )#.configure_legend(
-             #   orient='right'
-            #)
-        
-        scatter_plot_2 = alt.Chart(data).mark_circle(size=60).encode(
-            alt.X("Match:N", axis=alt.Axis(labels=True, ticks=True, domain=True, grid=True, domainColor="white", gridColor="white", labelColor="black", tickColor="white", titleColor="black")),
-            alt.Y("Predicted Score:Q", axis=alt.Axis(labels=True, ticks=True, domain=True, grid=True, domainColor="white", gridColor="white", labelColor="black", tickColor="white", titleColor="black")).scale(zero=False),
-            color = alt.value("orange"),
-            tooltip = ['Match', 'Match Name', 'Predicted Score', 'Alliance'] 
-            #alt.Color("variable:N", legend=alt.Legend(title="Score Type")),
-            ).properties(
-                width=200,
-                height=300
-            )
-            #.configure_legend(
-            #    orient='right'
-            #)
-
-        # Combine scatter plot and line of best fit
-        #line_of_fit = scatter_plot.transform_regression('Match','Points Scored').mark_line()
-        
-        combined_chart = scatter_plot_2 + scatter_plot_1
-
-        # Display the chart
-        st.altair_chart(combined_chart, use_container_width=True)
-        #st.altair_chart(scatter_plot_2, use_container_width=True)
 
 
 # Add teams dynamically
@@ -158,6 +87,8 @@ with tab1:
     # Display charts for each team
     for idx, tm in enumerate(teams_info):
         st.write("Team " + str(tm) + " Data")
+        filtered_data = data[data['teamNumber'] == tm]
+        st.dataframe(filtered_data)
 
 with tab2:
     st.header("Paper Scouting")
